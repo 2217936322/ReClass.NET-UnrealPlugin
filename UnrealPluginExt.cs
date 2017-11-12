045dcb0f487765b9a4152d388586e5ac47d05de9
Dr_Pepper <d0kt0rp3pp3r@gmail.com>
2017-11-12 08:34:26 +0100

    Formatting

--- UnrealPluginExt.cs
+++ UnrealPluginExt.cs
@@ -10,80 +10,80 @@ using static System.IO.Path;
 
 namespace UnrealPlugin
 {
-	public class UnrealPluginExt : Plugin, INodeInfoReader
-	{
-		private IPluginHost host;
-		internal static Settings Settings;
+    public class UnrealPluginExt : Plugin, INodeInfoReader
+    {
+        private IPluginHost host;
+        internal static Settings Settings;
 
-		private IntPtr gNames;
+        private IntPtr gNames;
         private IntPtr gObjects;
 
         public override bool Initialize(IPluginHost host)
-		{
-			//System.Diagnostics.Debugger.Launch();
+        {
+            //System.Diagnostics.Debugger.Launch();
 
-			if (this.host != null)
-			{
-				Terminate();
-			}
+            if (this.host != null)
+            {
+                Terminate();
+            }
 
-			this.host = host ?? throw new ArgumentNullException(nameof(host));
+            this.host = host ?? throw new ArgumentNullException(nameof(host));
 
-			Settings = host.Settings;
+            Settings = host.Settings;
 
-			// Register the InfoReader
-			host.RegisterNodeInfoReader(this);
+            // Register the InfoReader
+            host.RegisterNodeInfoReader(this);
 
-			// Register ProcessAttached handler
-			host.Process.ProcessAttached += OnProcessAttached;
+            // Register ProcessAttached handler
+            host.Process.ProcessAttached += OnProcessAttached;
 
-			return true;
-		}
+            return true;
+        }
 
-		private static IntPtr FindPattern(RemoteProcess process, Module module, string pattern)
-		{
-			// Read Module Bytes
-			var moduleBytes = process.ReadRemoteMemory(module.Start, module.Size.ToInt32());
+        private static IntPtr FindPattern(RemoteProcess process, Module module, string pattern)
+        {
+            // Read Module Bytes
+            var moduleBytes = process.ReadRemoteMemory(module.Start, module.Size.ToInt32());
 
-			// Parse Bytepattern
-			var bytePattern = BytePattern.Parse(pattern);
+            // Parse Bytepattern
+            var bytePattern = BytePattern.Parse(pattern);
 
-			// Find Bytepattern in our copy
-			var limit = moduleBytes.Length - bytePattern.Length;
-			for (var i = 0; i < limit; ++i)
-			{
-				if (bytePattern.Equals(moduleBytes, i))
-				{
-					return module.Start + i;
-				}
-			}
+            // Find Bytepattern in our copy
+            var limit = moduleBytes.Length - bytePattern.Length;
+            for (var i = 0; i < limit; ++i)
+            {
+                if (bytePattern.Equals(moduleBytes, i))
+                {
+                    return module.Start + i;
+                }
+            }
 
-			return IntPtr.Zero;
-		}
+            return IntPtr.Zero;
+        }
 
-		private void OnProcessAttached(RemoteProcess process)
-		{
-			process.UpdateProcessInformations();
+        private void OnProcessAttached(RemoteProcess process)
+        {
+            process.UpdateProcessInformations();
 
-			gNames = IntPtr.Zero;
+            gNames = IntPtr.Zero;
             gObjects = IntPtr.Zero;
 
             var processName = GetFileName(process.UnderlayingProcess.Path).ToLower();
-			switch (processName)
-			{
+            switch (processName)
+            {
                 // TODO: Add more games
-                
+
                 case "tslgame.exe": // Playerunknown's Battlegrounds
                 case "fortniteclient-win64-shipping.exe": // Fortnite
                     {
-						var pattern = "48 89 1D ?? ?? ?? ?? 48 8B 5C 24 ?? 48 83 C4 28 C3 48 8B 5C 24 ?? 48 89 05 ?? ?? ?? ?? 48 83 C4 28 C3";
-						var address = FindPattern(process, process.GetModuleByName(processName), pattern);
+                        var pattern = "48 89 1D ?? ?? ?? ?? 48 8B 5C 24 ?? 48 83 C4 28 C3 48 8B 5C 24 ?? 48 89 05 ?? ?? ?? ?? 48 83 C4 28 C3";
+                        var address = FindPattern(process, process.GetModuleByName(processName), pattern);
 
-						if (!address.IsNull())
-						{
-							var offset = process.ReadRemoteInt32(address + 0x3);
-							gNames = process.ReadRemoteIntPtr(address + offset + 7);
-						}
+                        if (!address.IsNull())
+                        {
+                            var offset = process.ReadRemoteInt32(address + 0x3);
+                            gNames = process.ReadRemoteIntPtr(address + offset + 7);
+                        }
 
                         pattern = "48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B D6 48 89 B5";
                         address = FindPattern(process, process.GetModuleByName(processName), pattern);
@@ -95,24 +95,24 @@ namespace UnrealPlugin
                         }
 
                         break;
-					}
-			}
-		}
+                    }
+            }
+        }
 
-		public override void Terminate()
-		{
-			host.DeregisterNodeInfoReader(this);
-		}
+        public override void Terminate()
+        {
+            host.DeregisterNodeInfoReader(this);
+        }
 
-		public string ReadNodeInfo(BaseNode node, IntPtr value, MemoryBuffer memory)
-		{
+        public string ReadNodeInfo(BaseNode node, IntPtr value, MemoryBuffer memory)
+        {
             if (IsUObject(value, memory))
             {
                 return GetUObjectName(value, memory);
             }
 
             return null;
-		}
+        }
 
         private bool IsUObject(IntPtr objectPtr, MemoryBuffer memory)
         {
@@ -140,12 +140,12 @@ namespace UnrealPlugin
             return result;
         }
 
-		private string GetUObjectName(IntPtr objectPtr, MemoryBuffer memory)
-		{
-			if (gNames.IsNull())
-			{
-				return null;
-			}
+        private string GetUObjectName(IntPtr objectPtr, MemoryBuffer memory)
+        {
+            if (gNames.IsNull())
+            {
+                return null;
+            }
 
 #if RECLASSNET64
             var nameIndex = memory.Process.ReadRemoteInt32(objectPtr + 0x18);
@@ -159,33 +159,33 @@ namespace UnrealPlugin
             }
 
             var numElements = memory.Process.ReadRemoteInt32(gNames + 0x80 * IntPtr.Size);
-			var numChunks = memory.Process.ReadRemoteInt32(gNames + 0x80 * IntPtr.Size + 0x4);
+            var numChunks = memory.Process.ReadRemoteInt32(gNames + 0x80 * IntPtr.Size + 0x4);
 
-			var indexChunk = nameIndex / 16384;
-			var indexName = nameIndex % 16384;
+            var indexChunk = nameIndex / 16384;
+            var indexName = nameIndex % 16384;
 
-			if (nameIndex < numElements && indexChunk < numChunks)
-			{
-				var chunkPtr = memory.Process.ReadRemoteIntPtr(gNames + indexChunk * IntPtr.Size);
+            if (nameIndex < numElements && indexChunk < numChunks)
+            {
+                var chunkPtr = memory.Process.ReadRemoteIntPtr(gNames + indexChunk * IntPtr.Size);
 
-				if (chunkPtr.MayBeValid())
-				{
-					var namePtr = memory.Process.ReadRemoteIntPtr(chunkPtr + indexName * IntPtr.Size);
+                if (chunkPtr.MayBeValid())
+                {
+                    var namePtr = memory.Process.ReadRemoteIntPtr(chunkPtr + indexName * IntPtr.Size);
 
-					var nameEntryIndex = memory.Process.ReadRemoteInt32(namePtr);
+                    var nameEntryIndex = memory.Process.ReadRemoteInt32(namePtr);
 
-					if (nameEntryIndex >> 1 == nameIndex)
-					{
-						var wideChar = (nameEntryIndex & 1) != 0;
+                    if (nameEntryIndex >> 1 == nameIndex)
+                    {
+                        var wideChar = (nameEntryIndex & 1) != 0;
 
-						var name = memory.Process.ReadRemoteString(wideChar ? Encoding.Unicode : Encoding.ASCII, namePtr + 0x8 + IntPtr.Size, 1024);
+                        var name = memory.Process.ReadRemoteString(wideChar ? Encoding.Unicode : Encoding.ASCII, namePtr + 0x8 + IntPtr.Size, 1024);
 
-						return name;
-					}
-				}
-			}
+                        return name;
+                    }
+                }
+            }
 
-			return null;
-		}
-	}
+            return null;
+        }
+    }
 }

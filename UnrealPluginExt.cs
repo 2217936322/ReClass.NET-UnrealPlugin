d333b34ecf15facf79692ff3548029a66f14062f
Dr_Pepper <d0kt0rp3pp3r@gmail.com>
2017-11-12 08:32:26 +0100

    Additional checks & Fortnite Support

--- UnrealPluginExt.cs
+++ UnrealPluginExt.cs
@@ -16,8 +16,9 @@ namespace UnrealPlugin
 		internal static Settings Settings;
 
 		private IntPtr gNames;
+        private IntPtr gObjects;
 
-		public override bool Initialize(IPluginHost host)
+        public override bool Initialize(IPluginHost host)
 		{
 			//System.Diagnostics.Debugger.Launch();
 
@@ -65,14 +66,16 @@ namespace UnrealPlugin
 			process.UpdateProcessInformations();
 
 			gNames = IntPtr.Zero;
+            gObjects = IntPtr.Zero;
 
-			var processName = GetFileName(process.UnderlayingProcess.Path).ToLower();
+            var processName = GetFileName(process.UnderlayingProcess.Path).ToLower();
 			switch (processName)
 			{
-				// TODO: Add more games
-
-				case "tslgame.exe": // Playerunknown's Battlegrounds
-					{
+                // TODO: Add more games
+                
+                case "tslgame.exe": // Playerunknown's Battlegrounds
+                case "fortniteclient-win64-shipping.exe": // Fortnite
+                    {
 						var pattern = "48 89 1D ?? ?? ?? ?? 48 8B 5C 24 ?? 48 83 C4 28 C3 48 8B 5C 24 ?? 48 89 05 ?? ?? ?? ?? 48 83 C4 28 C3";
 						var address = FindPattern(process, process.GetModuleByName(processName), pattern);
 
@@ -82,7 +85,16 @@ namespace UnrealPlugin
 							gNames = process.ReadRemoteIntPtr(address + offset + 7);
 						}
 
-						break;
+                        pattern = "48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B D6 48 89 B5";
+                        address = FindPattern(process, process.GetModuleByName(processName), pattern);
+
+                        if (!address.IsNull())
+                        {
+                            var offset = process.ReadRemoteInt32(address + 0x3);
+                            gObjects = address + offset + 7;
+                        }
+
+                        break;
 					}
 			}
 		}
@@ -94,32 +106,59 @@ namespace UnrealPlugin
 
 		public string ReadNodeInfo(BaseNode node, IntPtr value, MemoryBuffer memory)
 		{
+            if (IsUObject(value, memory))
+            {
+                return GetUObjectName(value, memory);
+            }
+
+            return null;
+		}
+
+        private bool IsUObject(IntPtr objectPtr, MemoryBuffer memory)
+        {
+            if (gObjects.IsNull())
+            {
+                return false;
+            }
+
+            var internalIndex = memory.Process.ReadRemoteInt32(objectPtr + IntPtr.Size + 0x4);
+
+            var numElements = memory.Process.ReadRemoteInt32(gObjects + 0x10 + IntPtr.Size + 0x4);
+
+            if (internalIndex < 0 || internalIndex >= numElements) return false;
+
+            var objects = memory.Process.ReadRemoteIntPtr(gObjects + 0x10);
+
+#if RECLASSNET64
+            var objectPtrCheck = memory.Process.ReadRemoteIntPtr(objects + internalIndex * 0x18);
+#else
+			var objectPtrCheck = memory.Process.ReadRemoteIntPtr(objects + internalIndex * 0x10);
+#endif
+
+            bool result = objectPtr == objectPtrCheck;
+
+            return result;
+        }
+
+		private string GetUObjectName(IntPtr objectPtr, MemoryBuffer memory)
+		{
 			if (gNames.IsNull())
 			{
 				return null;
 			}
 
 #if RECLASSNET64
-			var nameIndex = memory.Process.ReadRemoteInt32(value + 0x18);
+            var nameIndex = memory.Process.ReadRemoteInt32(objectPtr + 0x18);
 #else
-			var nameIndex = memory.Process.ReadRemoteInt32(value + 0x10);
+			var nameIndex = memory.Process.ReadRemoteInt32(objectPtr + 0x10);
 #endif
-			return ReadNameIndex(nameIndex, memory);
-		}
 
-		private string ReadNameIndex(int nameIndex, MemoryBuffer memory)
-		{
-			if (nameIndex < 1)
-			{
-				return null;
-			}
+            if (nameIndex < 1)
+            {
+                return null;
+            }
 
-			if (gNames.IsNull())
-			{
-				return null;
-			}
-
-			var numElements = memory.Process.ReadRemoteInt32(gNames + 0x80 * IntPtr.Size);
+            var numElements = memory.Process.ReadRemoteInt32(gNames + 0x80 * IntPtr.Size);
 			var numChunks = memory.Process.ReadRemoteInt32(gNames + 0x80 * IntPtr.Size + 0x4);
 
 			var indexChunk = nameIndex / 16384;
